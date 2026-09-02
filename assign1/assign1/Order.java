abb22ef023cf0ff28f1efa79c80d8d78692c224f
Bryan Ramirez <rbryan01@uafs.edu>
2026-09-01 20:33:51 -0500

    Placed a temporary rand int in orderDB so i could test Driver.

--- assign1/assign1/Order.java
+++ assign1/assign1/Order.java
@@ -1,5 +1,22 @@
 package assign1;
 
 public class Order {
+	int id;
+	String customer;
+	String product;
+	double amt;
+	String date;
+	
+	public Order(){
+		
+	}
+	
+	public Order(int id, String customer, String product, double amt, String date){
+		this.id=id;
+		this.customer=customer;
+		this.product=product;
+		this.amt=amt;
+		this.date=date;
+	}
 
 }
