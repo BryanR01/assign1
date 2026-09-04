8757831268c35693bd7e442b783e00b2dc4bc350
Bryan Ramirez <rbryan01@uafs.edu>
2026-09-03 20:20:01 -0500

    Finished?

--- assign1/assign1/Driver.java
+++ assign1/assign1/Driver.java
@@ -12,7 +12,9 @@ public class Driver {
 		
 		while(choice!=0) {
 			System.out.printf("What would you like to do?: \n1.Show Orders \n2.Add Order at end"
-					+ "\n3.Add order at spot\n4.Clear \n5.Get order from spot \n6.Search by order ID \n" );
+					+ "\n3.Add order at spot\n4.Clear \n5.Get order from spot \n6.Search by order ID "
+					+ "\n7.Load orders \n8.Remove order at spot \n9.Roplace order at spot "
+					+ "\n10.See amount of orders in list \n11.see total space on list \n12.Resize list +25\n" );
 			choice=key.nextInt();
 			
 			if(choice==1) {//1
@@ -100,7 +102,7 @@ public class Driver {
 				System.out.printf("%-9s %-35s %-9s", "--------", "--------", "---------\n");
 				System.out.printf("%-9s %-35s %-9s\n\n", getOrd.id, getOrd.product, getOrd.amt);
 			}
-			else if(choice==6) {
+			else if(choice==6) {//6
 				int id;
 				System.out.println("Enter order ID: ");
 				id=key.nextInt();
@@ -113,6 +115,63 @@ public class Driver {
 				
 				
 			}
+			else if(choice==7) {//7
+				data.loadOrders("orders.txt");
+			}
+			else if(choice==8) {//8
+				int position;
+				System.out.println("Enter position in list for order: ");
+				position=key.nextInt();
+				
+				data.remove(position);
+				
+				
+			}
+			else if(choice==9) {//9
+				int position;
+				
+				int id;
+				String customer;
+				String product;
+				double amt;
+				String date;
+				
+				System.out.println("Enter position in list for order: ");
+				position=key.nextInt();
+				
+				System.out.print("Enter Order ID: ");
+				id=key.nextInt();
+				key.nextLine();
+				
+				System.out.print("Enter Customer Name: ");
+				customer=key.nextLine();
+				
+				System.out.print("Enter Product name: ");
+				product=key.nextLine();
+				
+				
+				System.out.print("Enter Total Amount: ");
+				amt=key.nextDouble();
+				
+				
+				System.out.print("Enter Order Date: ");
+				date=key.next();
+				
+				Order newOrder=new Order(id, customer, product, amt, date);
+				data.set(position, newOrder);
+			}
+			else if(choice==10) {//10
+				int listItems=data.size;
+				System.out.println("Amount of items in list: "+listItems);
+			}
+			else if(choice==11) {
+				System.out.printf("Total capacity of list is: "+data.capacity()+"\n\n");
+			}
+			else if(choice==12) {
+				data.resize();
+			}
+	
+			
 			
 			
 		}//While
--- assign1/assign1/OrderDB.java
+++ assign1/assign1/OrderDB.java
@@ -20,25 +20,29 @@ public class OrderDB implements OrderDBInterface{
 			
 			scanner.nextLine();// !!!Might cause problems
 			
-			while (scanner.hasNextLine()) {
-				String line= scanner.nextLine();//Get one line from the file at once
-				String[] OrderInfo=line.split(",");// make an array with every part of the order
-				int id=Integer.parseInt(OrderInfo[0]);// The scanner made everything into a String so i have to change id and amt back to int and double
-				double amt=Double.parseDouble(OrderInfo[3]);
+			//while (scanner.hasNextLine()) {
+				for(int i=0; i<size; i++) {
+					String line= scanner.nextLine();//Get one line from the file at once
+					String[] OrderInfo=line.split(",");// make an array with every part of the order
+					int id=Integer.parseInt(OrderInfo[0]);// The scanner made everything into a String so i have to change id and amt back to int and double
+					double amt=Double.parseDouble(OrderInfo[3]);
+					
+					Order order= new Order(id, OrderInfo[1], OrderInfo[2], amt, OrderInfo[4]);
+					OrderAmt++;
+					
+						orders[i]=order;
+				}
 				
-				Order order= new Order(id, OrderInfo[1], OrderInfo[2], amt, OrderInfo[4]);
-				OrderAmt++;
-				
+				/*
 				if(rand<size) {
-				orders[place]=order;
+				orders[place]=order; //Original code used for testing
 				place++;
 				}
 				
 				rand++;
+				*/
 				
-				
-				
-			}
+			//}
 			
 		}catch(FileNotFoundException e) {
 			System.out.printf("Error: File - "+ fileName+ " - Not Found");
@@ -59,11 +63,14 @@ public class OrderDB implements OrderDBInterface{
 		try {
 		System.out.printf("%-9s %-35s %-9s", "Order ID", "Product","Total Amt\n");
 		System.out.printf("%-9s %-35s %-9s", "--------", "--------", "---------\n");
+		
 		for(int i=0; i<orders.length; i++) {
-			System.out.printf("%-9s %-35s %9.2f\n",orders[i].id , orders[i].product , orders[i].amt);
+			if(orders[i]!=null) {
+				System.out.printf("%-9s %-35s %9.2f\n",orders[i].id , orders[i].product , orders[i].amt);
+			}
 		}
 		}catch(NullPointerException e) {
-			System.out.printf("List is emplty\n\n");
+			System.out.printf("Array size error\n\n");
 		}
 		
 	}//Show
@@ -80,7 +87,11 @@ public class OrderDB implements OrderDBInterface{
 
 	@Override
 	public void add(int index, Order order) {
-		orders[index-1]=order;
+		if(orders[index]==null) {
+			orders[index-1]=order;
+		}else {
+			System.out.println("That spot has an order in it. Try option 9 to replace it.");
+		}
 		
 	}
 
@@ -113,33 +124,46 @@ public class OrderDB implements OrderDBInterface{
 
 	@Override
 	public Order remove(int index) {
-		// TODO Auto-generated method stub
-		return null;
+		Order removedOrd=orders[index-1];
+		orders[index-1]=null;
+		
+		System.out.println("Removed order: "+removedOrd.id+"-"+removedOrd.product);
+		
+		return removedOrd;
 	}
 
 	@Override
 	public Order set(int index, Order order) {
-		// TODO Auto-generated method stub
-		return null;
+		Order replacedOrd=orders[index-1];
+		orders[index-1]=order;
+		
+		
+		return replacedOrd;
 	}
 
 	@Override
 	public int size() {
-		// TODO Auto-generated method stub
-		return 0;
+		int listItems=0;
+		for(int i=0; i<orders.length; i++) {
+			if(orders[i]!=null) {
+				listItems++;
+			}
+		}
+		return listItems;
 	}
 
 	@Override
 	public int capacity() {
-		// TODO Auto-generated method stub
-		return 0;
+		int cap=orders.length;
+		return cap;
 	}
 
 	@Override
 	public void resize() {
 		Order[] ordersTemp=orders;
-		Order[] orders= new Order[size+25];
-		for(int i=0; i>size; i++) {
+		Order[] newOrders= new Order[size+25];
+		this.orders=newOrders;
+		for(int i=0; i>orders.length; i++) {
 			orders[i]=ordersTemp[i];
 		}
 		size+=25;
